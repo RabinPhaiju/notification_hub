758108b2be7d44ebdfd19e211d7855eedf6c345a
Rabin Phaiju <rabinphaiju15@gmail.com>
2024-12-31 10:02:18 +0545

    added drf

--- base/views.py
+++ base/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from .models import Notification
+from .serializers import NotificationSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework import viewsets,status
+from rest_framework.response import Response
 
-# Create your views here.
+class NotificationViewSet(viewsets.ModelViewSet):
+    permission_classes = [AllowAny]
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
+
+    def create(self, request, *args, **kwargs):
+        return Response({"detail": "Method not allowed."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def update(self, request, *args, **kwargs):
+        return Response({"detail": "Method not allowed."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def destroy(self, request, *args, **kwargs):
+        return Response({"detail": "Method not allowed."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
--- notification_hub/urls.py
+++ notification_hub/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from base.views import NotificationViewSet
+
+router = DefaultRouter()
+router.register(r'notifications', NotificationViewSet, basename='notification')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+]
+
+urlpatterns += [
+    path('api/', include(router.urls)),
 ]
